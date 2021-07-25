e68c7fa29c96a374dd366e10891a005e4747edc3
Paulo Ribeiro <40372913+pauloribeiro93@users.noreply.github.com>
2021-07-25 12:59:37 -0300

    Update BaseTest.cs

--- Common/BaseTest.cs
+++ Common/BaseTest.cs
@@ -10,31 +10,25 @@ using OpenQA.Selenium.Chrome;
 namespace NinjaPlus.Common
 {
     public class BaseTest
-    //publica =>public == Qualuqer codigo acessa ou projeto exemplo nome
-    //privada =>private == Ela pode ser acessa somente pela classe  que ela está _nome
-    //protegida => protected == Ela pode ser acessada somente por ela ou por um filho herdado Nome
+
     {
-        protected BrowserSession Browser; //Propriedade para eu poder usar em todo test
+        protected BrowserSession Browser; 
 
 
-        [SetUp] //Before do nUnit | Instanciei o Coypu para o Setup.
+        [SetUp] 
         public void Setup()
         {
             var config = new ConfigurationBuilder()
             .AddJsonFile("config.json")
             .Build();
-            // 2 - Aqui é segunda coisa a se fazer
-            //Aqui realizamos as configurações de acesso igual o capybara // Configurações do Coypu definidas na varivel(Config)
+          
             var sessionConfig = new SessionConfiguration
             {
-                AppHost = "http://ninjaplus-web", //Definir host pafrão.
-                Port = 5000, //porta padrão
-                SSL = false, //Propriedade SSL define se vai trabalhar com HTTPS.
-                Driver = typeof(SeleniumWebDriver), //Tipo de Driver utilizado, Necessario realizar 
-                //o using para SeleniumWebDriver.
-                // Browser = Coypu.Drivers.Browser.Chrome, //Decisão do Navegador a ser utiizado no teste. 
-                Timeout = TimeSpan.FromSeconds(10), //timout padrão
-                
+                AppHost = "http://ninjaplus-web",
+                Port = 5000, 
+                SSL = false,
+                Driver = typeof(SeleniumWebDriver),
+                Timeout = TimeSpan.FromSeconds(10),
 
             };
             if (config["browser"].Equals("chrome"))
@@ -46,10 +40,8 @@ namespace NinjaPlus.Common
             {
                 sessionConfig.Browser = Coypu.Drivers.Browser.Firefox;
             }
-            // 1 - TUDO COMEÇA AQUI
-            //Quando eu estanciar Browser aqui em baixo eu vou passar as configurações(configs) para BrowserSession
-            //Minha variavel Browser sabe qual configurações eu vou utilizar(As informações do config)
-            Browser = new BrowserSession(sessionConfig); //instanciei para utilizar os configs | BrowserSession é onde tudo começa, vou ter acesso do coypu aqui 
+            
+            Browser = new BrowserSession(sessionConfig);
             Browser.MaximiseWindow();
         }
 
@@ -63,7 +55,7 @@ namespace NinjaPlus.Common
         }
         public void TakeScreenshot()
         {
-            var resultId = TestContext.CurrentContext.Test.ID; // Contexto do NUnit
+            var resultId = TestContext.CurrentContext.Test.ID; 
             var shotPath = Environment.CurrentDirectory + "\\Screenshots";
 
             if (!Directory.Exists(shotPath))
@@ -79,8 +71,8 @@ namespace NinjaPlus.Common
         }
 
 
-        //Afater do nUnit
-        [TearDown] //Gancho| independente o que acontecer no Teste ele vai fechar o Browser
+       
+        [TearDown] 
         public void Finish()
         {
             try
