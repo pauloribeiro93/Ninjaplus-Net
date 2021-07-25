41f33e4b023d3d86690b54935fa9ce83b3f95de3
Paulo Ribeiro <40372913+pauloribeiro93@users.noreply.github.com>
2021-07-25 12:57:11 -0300

    Update LoginPage.cs

--- Pages/LoginPage.cs
+++ Pages/LoginPage.cs
@@ -5,30 +5,24 @@ namespace NinjaPlus.Pages
 {
     public class LoginPage
     {
-        //1 - Coisa para se fazer: FOi criado para poder ter acesso a estancia do Coypu(browser)
-        //  |
-        //  V
-        private readonly BrowserSession _browser; //_POr convenção é para com under_line
-
-
-        //3 - FOi criado o construtor para a instancia funcionar.
-        //                                 |
-        //                                 V
-        public LoginPage(BrowserSession Browser) //Construtor é um metodo que tem o mesmo nome da classe. 
-                                                 //O constutor não é tipado, sendo assim, ele vai receber um Objeto do tipo (BrowserSession browser).
-                                                 //Sendo assim, vou poder usar ele para  Para realizar as buscas
+    
+        private readonly BrowserSession _browser; 
+        
+        public LoginPage(BrowserSession Browser)  
+                                                 
+                                                
         {
-            _browser = Browser; //<===Quando instaciar a classe login page vou falar que _browser = browser  
-                                // Dessa forma eu vou receber a instacia do coypu dentro dessa classe 
-                                //vou poder usar o _browser para buscar os elementos
+            _browser = Browser; 
+                              
+                                
         }
         public void Load()
         {
             _browser.Visit("/login");
         }
-        public void Whith(string email, string pass) //Custom command ou custom action
+        public void Whith(string email, string pass)
         {
-            this.Load(); //This assim eu invoco o metodo dentro de outro metodo ou class
+            this.Load(); 
             _browser.FillIn("emailId").With(email);
             _browser.FindCss("#passId").SendKeys(pass);
             _browser.ClickButton("Entrar");
